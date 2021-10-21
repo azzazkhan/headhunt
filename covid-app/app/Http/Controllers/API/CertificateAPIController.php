b914df7ccb9123469a19a2ed4449a889ea31ce61
Azzaz Khan <students.mails9@gmail.com>
2021-10-21 18:08:05 +0500

    Fixed file discovery issue in CertificateAPIController@store second attempt

--- covid-app/app/Http/Controllers/API/CertificateAPIController.php
+++ covid-app/app/Http/Controllers/API/CertificateAPIController.php
@@ -21,7 +21,7 @@ class CertificateAPIController extends Controller
         $user = auth('api')->user();
 
         // Check if file was successfully uploaded
-        if (! ($request->hasFile('certificate') && !$request->file('certificate')->isValid()))
+        if (! $request->hasFile('certificate'))
             return abort(400, 'Invalid or corrupt file provided!');
 
         
