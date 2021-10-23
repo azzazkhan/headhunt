b1733740578c2aa854a317ced1700133df0e00f0
Azzaz Khan <students.mails9@gmail.com>
2021-10-24 00:35:28 +0500

    Updated certificate data table

--- covid-app/app/DataTables/CertificateDataTable.php
+++ covid-app/app/DataTables/CertificateDataTable.php
@@ -2,36 +2,45 @@
 
 namespace App\DataTables;
 
-use App\Certificate;
-use Yajra\DataTables\Html\Button;
-use Yajra\DataTables\Html\Column;
-use Yajra\DataTables\Html\Editor\Editor;
-use Yajra\DataTables\Html\Editor\Fields;
-use Yajra\DataTables\Services\DataTable;
-use Yajra\DataTables\EloquentDataTable;
+use App\Models\Certificate;
+use App\Models\CustomField;
+use App\Models\Post;
 use Barryvdh\DomPDF\Facade as PDF;
+use Yajra\DataTables\DataTableAbstract;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder;
+use Yajra\DataTables\Services\DataTable;
 
 class CertificateDataTable extends DataTable
 {
     /**
+     * custom fields columns
+     * @var array
+     */
+    public static $customFields = [];
+
+    /**
      * Build DataTable class.
      *
      * @param mixed $query Results from query() method.
-     * @return \Yajra\DataTables\DataTableAbstract
+     * @return DataTableAbstract
      */
     public function dataTable($query)
     {
         $dataTable = new EloquentDataTable($query);
         $columns = array_column($this->getColumns(), 'data');
         $dataTable = $dataTable
-            ->editColumn('image', function ($category) {
-                return '<img class="rounded" style="height: 50px;" src="' . url(Storage::url('certificates/' . $certificate->ref . '.jpg')) . '" alt="Certificate for ' . $category->user->name .'">';
+            ->editColumn('image', function ($certificate) {
+                return getMediaColumn($certificate, 'image');
             })
-            ->editColumn('status', function ($category) {
-                return $category->status;
+            ->editColumn('name', function ($category) {
+                return $category->name;
             })
             ->editColumn('user', function ($category) {
                 return $category->user->name;
+            })
+            ->editColumn('status', function ($category) {
+                return $category->status;
             })
             ->editColumn('created_at', function ($category) {
                 return $category->created_at;
@@ -39,53 +48,7 @@ class CertificateDataTable extends DataTable
             ->addColumn('action', 'certificates.datatables_actions')
             ->rawColumns(array_merge($columns, ['action']));
 
-
         return $dataTable;
-    }
-
-    /**
-     * Get query source of dataTable.
-     *
-     * @param \App\Certificate $model
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function query(Certificate $model)
-    {
-        return $model->newQuery()->select('certificates.*');
-    }
-
-    /**
-     * Optional method if you want to use html builder.
-     *
-     * @return \Yajra\DataTables\Html\Builder
-     */
-    public function html()
-    {
-        return $this->builder()
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    ->addAction(['width' => '80px', 'printable' => false, 'responsivePriority' => '100'])
-                    ->setTableId('certificates-table')
-                    ->dom('Bfrtip')
-                    ->orderBy(1);
-                    // ->buttons(
-                    //     Button::make('create'),
-                    //     Button::make('export'),
-                    //     Button::make('print'),
-                    //     Button::make('reset'),
-                    //     Button::make('reload')
-                    // );
-    }
-
-    /**
-     * Export PDF using DomPDF
-     * @return mixed
-     */
-    public function pdf()
-    {
-        $data = $this->getDataForPrint();
-        $pdf = PDF::loadView($this->printPreview, compact('data'));
-        return $pdf->download($this->filename() . '.pdf');
     }
 
     /**
@@ -98,44 +61,81 @@ class CertificateDataTable extends DataTable
         $columns = [
             [
                 'data' => 'image',
-                'title' => 'Thumbnail',
-                'searchable' => false,
-                'orderable' => false,
-                'exportable' => false,
-            ],
-            [
-                'data' => 'status',
-                'title' => 'Status',
+                'title' => trans('lang.category_image'),
+                'searchable' => false, 'orderable' => false, 'exportable' => false, 'printable' => false,
             ],
             [
                 'data' => 'user',
-                'title' => 'Provider Name',
+                'title' => 'Provider Name'
+
+            ],
+            [
+                'data' => 'status',
+                'title' => 'Status'
+
             ],
             [
                 'data' => 'created_at',
-                'title' => 'Submitted At',
+                'title' => 'Submitted On',
+                'searchable' => false,
             ]
         ];
 
+        $hasCustomField = in_array(Certificate::class, setting('custom_field_models', []));
+        if ($hasCustomField) {
+            $customFieldsCollection = CustomField::where('custom_field_model', Certificate::class)->where('in_table', '=', true)->get();
+            foreach ($customFieldsCollection as $key => $field) {
+                array_splice($columns, $field->order - 1, 0, [[
+                    'data' => 'custom_fields.' . $field->name . '.view',
+                    'title' => $field->name,
+                    'orderable' => false,
+                    'searchable' => false,
+                ]]);
+            }
+        }
         return $columns;
+    }
 
-        // return [
-        //     Column::computed('action')
-        //           ->exportable(false)
-        //           ->printable(false)
-        //           ->width(60)
-        //           ->addClass('text-center'),
-        //     Column::make('id'),
-        //     Column::make('image')
-        //           ->exportable(false)
-        //           ->width(60)
-        //           ->addClass('text-center')
-        //           ->searchable(false)
-        //           ->orderable(false),
-        //     Column::make('ref'),
-        //     Column::make('status'),
-        //     Column::make('created_at'),
-        // ];
+    /**
+     * Get query source of dataTable.
+     *
+     * @param Certificate $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Certificate $model)
+    {
+        return $model->newQuery()->select("certificates.*");
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->addAction(['width' => '80px', 'printable' => false, 'responsivePriority' => '100'])
+            ->parameters(array_merge(
+                config('datatables-buttons.parameters'), [
+                    'language' => json_decode(
+                        file_get_contents(base_path('resources/lang/' . app()->getLocale() . '/datatable.json')
+                        ), true)
+                ]
+            ));
+    }
+
+    /**
+     * Export PDF using DOMPDF
+     * @return mixed
+     */
+    public function pdf()
+    {
+        $data = $this->getDataForPrint();
+        $pdf = PDF::loadView($this->printPreview, compact('data'));
+        return $pdf->download($this->filename() . '.pdf');
     }
 
     /**
@@ -145,6 +145,6 @@ class CertificateDataTable extends DataTable
      */
     protected function filename()
     {
-        return 'Certificates_' . date('YmdHis');
+        return 'Certificates_' . time();
     }
 }
